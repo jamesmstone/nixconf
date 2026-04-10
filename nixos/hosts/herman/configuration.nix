f4271a771a31bdce3e93d674237dbe2919fdf26e
James <darter@jamesst.one>
2026-04-10 17:49:48 +0200

    add herman

---
+++ nixos/hosts/herman/configuration.nix
@@ -0,0 +1,31 @@
+{
+  inputs,
+  self,
+  lib,
+  ...
+}: {
+  flake.nixosConfigurations.herman = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      self.nixosModules.hostHerman
+    ];
+  };
+
+  flake.nixosModules.hostHerman = {pkgs, ...}: {
+    imports = [
+      self.nixosModules.base
+      self.nixosModules.general
+      self.nixosModules.desktop
+    ];
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "herman";
+
+    networking.networkmanager.enable = true;
+
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+    system.stateVersion = "25.11";
+  };
+}
---
+++ nixos/hosts/herman/hardware-configuration.nix
@@ -0,0 +1,38 @@
+{
+  flake.nixosModules.hostHerman = {
+    config,
+    lib,
+    pkgs,
+    modulesPath,
+    ...
+  }: {
+    imports = [
+      (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+    boot.initrd.availableKernelModules = ["xhci_pci" "nvme" "usb_storage" "sd_mod"];
+    boot.initrd.kernelModules = [];
+    boot.kernelModules = ["kvm-intel"];
+    boot.extraModulePackages = [];
+
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/59fcfb83-c2f0-49f1-ae91-c5102d215e59";
+      fsType = "ext4";
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/492E-F333";
+      fsType = "vfat";
+      options = ["fmask=0022" "dmask=0022"];
+    };
+
+    swapDevices = [
+      {device = "/dev/disk/by-uuid/16f0143a-50a3-4ac7-87f4-da97179f85f8";}
+    ];
+
+    networking.useDHCP = lib.mkDefault true;
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
+}
