88eed9e2850f0d21e57c64e5c645bb195d0192c2
James <darter@jamesst.one>
2026-04-16 13:22:59 +0200

    Update Pixel 9a configuration: replace `inputs.nixpkgs` with `nixpkgs-droid`

--- nixos/hosts/pixel9a/configuration.nix
+++ nixos/hosts/pixel9a/configuration.nix
@@ -1,8 +1,12 @@
 {inputs, ...}: {
   flake.nixOnDroidConfigurations.pixel9a = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-    pkgs = import inputs.nixpkgs {system = "aarch64-linux";};
+    pkgs = import nixpkgs-droid {system = "aarch64-linux";};
     modules = [
-      ({pkgs, lib, ...}: {
+      ({
+        pkgs,
+        lib,
+        ...
+      }: {
         user.shell = lib.getExe pkgs.fish;
 
         environment.packages = with pkgs; [
