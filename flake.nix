18ed4d93ce14e8d499bbdfb35e0802c4c25490e2
James <darter@jamesst.one>
2026-04-16 13:25:06 +0200

    Update flake.nix: rename `nixpkgs-unstable-droid` to `nixpkgs-nixpkgs-droid`

--- flake.nix
+++ flake.nix
@@ -34,11 +34,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixpkgs-unstable-droid.url = "github:NixOS/nixpkgs/88d3861";
+    nixpkgs-nixpkgs-droid.url = "github:NixOS/nixpkgs/88d3861";
 
     nix-on-droid = {
       url = "github:nix-community/nix-on-droid/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs-unstable-droid";
+      inputs.nixpkgs.follows = "nixpkgs-nixpkgs-droid";
     };
   };
 
