d2104489e7d6ed91d12b45ac84b05489ff5a8473
James <darter@jamesst.one>
2026-04-17 16:08:29 +0200

    Update Pixel 9a configuration: enhance fish variant switcher with `all` option and debug output

--- nixos/hosts/pixel9a/configuration.nix
+++ nixos/hosts/pixel9a/configuration.nix
@@ -25,18 +25,58 @@
     # user.shell = lib.getExe selfpkgs.fish;  # TEMP: Comment out for testing
 
     # Deploy with fish-variant switcher for instant testing
-    environment.packages = with pkgs; [
-      # Fish variant switcher
-      pkgs.writeScriptBin "fv" ''
+    environment.packages = [
+      (pkgs.writeScriptBin "fv" ''
+        run_with_debug() {
+          echo "[DEBUG] Starting: $*"
+          ${pkgs.coreutils}/bin/timeout 5 "$@"
+          status=$?
+          if [ $status -ne 0 ]; then
+            echo "[ERROR] Command failed with status $status"
+          fi
+          echo "[DEBUG] Finished: $*"
+          echo
+          return $status
+        }
+
+        run_all() {
+          run_with_debug ${pkgs.fish}/bin/fish "$@"
+          run_with_debug ${selfpkgs.fish-test-simple}/bin/fish-test-simple "$@"
+          run_with_debug ${selfpkgs.fish-test-minimal}/bin/fish-test-minimal "$@"
+          run_with_debug ${selfpkgs.fish-test-zoxide}/bin/fish-test-zoxide "$@"
+          run_with_debug ${selfpkgs.fish-test-debug}/bin/fish-test-debug "$@"
+        }
+
         case "$1" in
-          s)   exec ${pkgs.fish}/bin/fish "$@" ;;
-          simple)   exec ${selfpkgs.fish-test-simple}/bin/fish-test-simple "$@" ;;
-          minimal)  exec ${selfpkgs.fish-test-minimal}/bin/fish-test-minimal "$@" ;;
-          oxide)    exec ${selfpkgs.fish-test-zoxide}/bin/fish-test-zoxide "$@" ;;
-          debug)    exec ${selfpkgs.fish-test-debug}/bin/fish-test-debug "$@" ;;
-          *)        exec ${selfpkgs.environment}/bin/fish "$@" ;;
+          all)
+            shift
+            run_all "$@"
+            ;;
+          s)
+            shift
+            exec ${pkgs.fish}/bin/fish "$@"
+            ;;
+          simple)
+            shift
+            exec ${selfpkgs.fish-test-simple}/bin/fish-test-simple "$@"
+            ;;
+          minimal)
+            shift
+            exec ${selfpkgs.fish-test-minimal}/bin/fish-test-minimal "$@"
+            ;;
+          oxide)
+            shift
+            exec ${selfpkgs.fish-test-zoxide}/bin/fish-test-zoxide "$@"
+            ;;
+          debug)
+            shift
+            exec ${selfpkgs.fish-test-debug}/bin/fish-test-debug "$@"
+            ;;
+          *)
+            exec ${selfpkgs.environment}/bin/fish "$@"
+            ;;
         esac
-      ''
+      '')
     ];
 
     nix.extraOptions = ''
